d3236097e50187f59d173eb95400aa68468d071e
Viren Khandal <virenkhandal@berkeley.edu>
2021-02-02 16:41:55 -0800

    cleanup cleanup everybody cleanup

--- requests/main.py
+++ requests/main.py
@@ -3,9 +3,9 @@ import requests
 payload = {'txtFirstName': 'Viren', 'last_name': 'Khandal'}
 autho = ('user', 'pass')
 
-r = requests.get('https://github.com/login', auth=autho)
+r = requests.get('https://github.com')
 
 # outfile = open('form.txt', 'wb')
 # outfile.write(r.content)
 
-print(r.ok)
+print(r.links)
