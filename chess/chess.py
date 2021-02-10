522fd23aa7902a53151ccda07f0e27049c12b049
Viren Khandal <virenkhandal@berkeley.edu>
2021-02-10 15:40:19 -0800

    added total games played

--- chess/chess.py
+++ chess/chess.py
@@ -31,13 +31,14 @@ for i in response.json().get("games"):
         pacific = utc_timestamp.astimezone(timezone('US/Pacific'))
         date_played = pacific.strftime(date_format)
         if not date_played in dates:
-            dates[date_played] = {'W': 0, 'L': 0, 'D': 0, 'times-white': 0, 'times-black': 0}
+            dates[date_played] = {'W': 0, 'L': 0, 'D': 0, 'total': 0,'times-white': 0, 'times-black': 0}
 
         #daily record
         white = i.get('white')
         black = i.get('black')
         if white.get('username') == username :
             dates.get(date_played)['times-white'] += 1
+            dates.get(date_played)['total'] += 1
             if white.get('result') in win:
                 dates.get(date_played)['W'] += 1
             elif white.get('result') in loss:
@@ -46,6 +47,7 @@ for i in response.json().get("games"):
                 dates.get(date_played)['D'] += 1
         else:
             dates.get(date_played)['times-black'] += 1
+            dates.get(date_played)['total'] += 1
             if black.get('result') in win:
                 dates.get(date_played)['W'] += 1
             elif black.get('result') in loss:
@@ -59,6 +61,7 @@ for i in response.json().get("games"):
 def print_nice(dates):
     for date_played, arr in dates.items():
         print("Stats for {0}:".format(date_played))
+        print("Total games played: {}".format(arr['total']))
         print("Wins: {}".format(arr['W']))
         print("Losses: {}".format(arr['L']))
         print("Draws: {}".format(arr['D']))
