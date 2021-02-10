9eb6c43ae96e6cc34cffb805cb03589afb3c9f30
Viren Khandal <virenkhandal@berkeley.edu>
2021-02-10 15:21:17 -0800

    populated dictionary with record and times white/black

--- chess/chess.py
+++ chess/chess.py
@@ -14,7 +14,9 @@ username = input("Enter your username: ")
 response = requests.get(endpoint + username + "/games/" + str(currentYear) + "/" + str(currentMonth))
 
 dates = {}
-
+win = ['win']
+loss = ['checkmated', 'timeout', 'resigned']
+draw = ['stalemate', 'timevsinsufficient', 'insufficient']
 
 for i in response.json().get("games"):
     game = i.get("pgn")
@@ -30,9 +32,30 @@ for i in response.json().get("games"):
         date_played = pacific.strftime(date_format)
         if not date_played in dates:
             dates[date_played] = {'W': 0, 'L': 0, 'D': 0, 'times-white': 0, 'times-black': 0}
-        
+
         #daily record
-        
+        white = i.get('white')
+        black = i.get('black')
+        if white.get('username') == username :
+            dates.get(date_played)['times-white'] += 1
+            if white.get('result') in win:
+                dates.get(date_played)['W'] += 1
+            elif white.get('result') in loss:
+                dates.get(date_played)['L'] += 1
+            elif white.get('result') in draw:
+                dates.get(date_played)['D'] += 1
+        else:
+            dates.get(date_played)['times-black'] += 1
+            if black.get('result') in win:
+                dates.get(date_played)['W'] += 1
+            elif black.get('result') in loss:
+                dates.get(date_played)['L'] += 1
+            elif black.get('result') in draw:
+                dates.get(date_played)['D'] += 1
+            
+        # print("White: " + i.get('white').get('result'))
+        # print("Black: " + i.get('black').get('result'))
+
 
 
 print(dates)
