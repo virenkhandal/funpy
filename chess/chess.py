bff79765b9caa76f302f24bfa6bb296fcbf3ba14
Viren Khandal <virenkhandal@berkeley.edu>
2021-02-10 00:40:36 -0800

    updating chess script to get dates of games

--- chess/chess.py
+++ chess/chess.py
@@ -1,20 +1,31 @@
 from datetime import datetime
 import requests
+from pytz import timezone
+import pytz
+from dateutil import parser
 
 currentMonth = datetime.now().month
 currentYear = datetime.now().year
 currentMonth = str(currentMonth).zfill(2)
+my_timezone=timezone('US/Pacific')
+date_format='%m/%d/%Y %H:%M:%S %Z'
+
 
 endpoint = "https://api.chess.com/pub/player/"
-
 username = input("Enter your username: ")
-
 payload = {"username": "viren2004"}
-
 games = requests.get(endpoint + username + "/games/" + str(currentYear) + "/" + str(currentMonth))
 
-fp = open("last30.txt", "wb")
-fp.write(games.content)
-# iterate over content (game)
-print("Done writing to file")
-# print(games.headers)
+
+for i in games.json().get("games"):
+    game = i.get("pgn")
+    if game is not None:
+        date = game.splitlines()[2][7:17]
+        time = game.splitlines()[12][10:18]
+        date_time_str = date + " " + time + " UTC"
+        
+        utc_date = parser.parse(date_time_str)
+
+        date = utc_date.astimezone(timezone('US/Pacific'))
+        print("Local date & time is: " + date.strftime(date_format))
+        
