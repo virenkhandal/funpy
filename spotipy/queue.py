91ce45e01d6aa64b63ba5f8ca770b9c15b368607
Viren Khandal <virenkhandal@berkeley.edu>
2021-03-01 01:33:43 -0800

    new top 10 music :O

--- spotipy/queue.py
+++
@@ -1,6 +0,0 @@
-import spotipy, requests
-from spotipy.oauth2 import SpotifyClientCredentials
-from private import client_id, secret, oauth
-
-
-post_endpoint = "https://api.spotify.com/v1/me/player/queue"
