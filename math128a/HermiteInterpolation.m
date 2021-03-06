bcc06f2ef44168e37a2c18ee7e2f84796a7c4ef0
Viren Khandal <virenkhandal@berkeley.edu>
2021-03-05 23:52:12 -0800

    added hermite interpolation script

---
+++ math128a/HermiteInterpolation.m
@@ -0,0 +1,21 @@
+function Q = HermiteInterpolation(x,f, f2)
+    [row, col] = size(x);
+    z(1:col*2) = 0;
+    Q(1:col*2, 1:col*2) = 0;
+    for i=1:col
+        z(2*i-1)=x(i);
+        z(2*i)=x(i);
+        Q(2*i-1, 1)=f(i);
+        Q(2*i, 1)=f(i);
+        Q(2*i, 2)=f2(i);
+        if (i ~= 1)
+            Q(2*i-1, 2)=(Q(2*i-1, 1)-Q(2*i-2,1))/(z(2*i-1)-z(2*i-2));
+        end
+    end
+    for i=3:2*col
+        for j=3:i
+            Q(i, j) = (Q(i, j-1)-Q(i-1, j-1))/(z(i)-z(i-j+1));
+        end
+    end
+    return;
+end
