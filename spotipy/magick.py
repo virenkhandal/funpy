22973c31de50271bf331a1e755b97b4d12affb67
Viren Khandal <virenkhandal@yahoo.com>
2021-06-23 00:10:11 -0700

    WOW

--- spotipy/magick.py
+++ spotipy/magick.py
@@ -1,24 +1,42 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def writetext():
+
+    # Image + Font Setup
     image = Image.open("igstory.png")
     draw = ImageDraw.Draw(image)
-    time = "weeks"
-    one = "Kid Cudi"
-    two = "Lana Del Rey"
-    three = "Machine Gun K..."
-    four = "Sad Alex"
-    five = "Migos"
     font = ImageFont.truetype(r'/System/Library/Fonts/Supplemental/Arial.ttf', 60) 
-    # font = ImageFont.load_default()
 
-    draw.text((120, 110), time, fill="black", font=font, align="left")
+    # Text to write onto image
+    time = "weeks"
+    artist_one = "Kid Cudi"
+    artist_two = "Lana Del Rey"
+    artist_three = "Machine Gun K..."
+    artist_four = "Sad Alex"
+    artist_five = "Migos"
 
-    draw.text((120, 1180), one, fill="black", font=font, align="left")
-    draw.text((120, 1320), two, fill="black", font=font, align="left")
-    draw.text((120, 1455), three, fill="black", font=font, align="left")
-    draw.text((120, 1595), four, fill="black", font=font, align="left")
-    draw.text((120, 1735), five, fill="black", font=font, align="left")
+    track_one = "Vibes (feat...)"
+    track_two = "Lana Del Rey"
+    track_three = "Machine Gu..."
+    track_four = "Sad Alex"
+    track_five = "Migos"
+    # Drawing text on image
+    draw.text((650, 200), time, fill="black", font=font, align="left")
+
+    draw.text((120, 1180), artist_one, fill="black", font=font, align="left")
+    draw.text((120, 1320), artist_two, fill="black", font=font, align="left")
+    draw.text((120, 1455), artist_three, fill="black", font=font, align="left")
+    draw.text((120, 1595), artist_four, fill="black", font=font, align="left")
+    draw.text((120, 1735), artist_five, fill="black", font=font, align="left")
+
+    draw.text((670, 1180), track_one, fill="black", font=font, align="left")
+    draw.text((670, 1320), track_two, fill="black", font=font, align="left")
+    draw.text((670, 1455), track_three, fill="black", font=font, align="left")
+    draw.text((670, 1595), track_four, fill="black", font=font, align="left")
+    draw.text((670, 1735), track_five, fill="black", font=font, align="left")
+
+
+    # Display image
     image.show()
 
 if __name__ == "__main__":
