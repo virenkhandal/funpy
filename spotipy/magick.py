4a6d0394f8e9b7a7dd1571b31120496ecc3e5829
Viren Khandal <virenkhandal@yahoo.com>
2021-06-22 22:53:55 -0700

    WOW

--- spotipy/magick.py
+++ spotipy/magick.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 def writetext():
     image = Image.open("igstory.png")
     draw = ImageDraw.Draw(image)
+    time = "weeks"
     one = "Kid Cudi"
     two = "Lana Del Rey"
     three = "Machine Gun K..."
@@ -10,6 +11,8 @@ def writetext():
     five = "Migos"
     font = ImageFont.truetype(r'/System/Library/Fonts/Supplemental/Arial.ttf', 60) 
     # font = ImageFont.load_default()
+
+    draw.text((120, 110), time, fill="black", font=font, align="left")
 
     draw.text((120, 1180), one, fill="black", font=font, align="left")
     draw.text((120, 1320), two, fill="black", font=font, align="left")
