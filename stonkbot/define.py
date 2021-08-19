63c368a017b817f939af2db3eaafe061bfab5af4
Viren Khandal <virenkhandal@berkeley.edu>
2021-08-18 20:41:23 -0700

    update

--- stonkbot/define.py
+++ stonkbot/define.py
@@ -2,6 +2,12 @@ from PyDictionary import PyDictionary
 
 dictionary=PyDictionary()
 
+def define(permuatations):
+    true_words = []
+    for i in tqdm(permuatations):
+        if dictionary.meaning(i, disable_errors=True) != None:
+            true_words.append(i)
+    return true_words
 
 if __name__ == "__main__":
     pass
